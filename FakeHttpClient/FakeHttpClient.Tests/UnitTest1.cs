30e9c5077754f11e0fb80cb6eafa9796fc4f06dc
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-01-27 17:57:01 +0530

    Implemented basic FakeHttpClient

--- FakeHttpClient/FakeHttpClient.Tests/UnitTest1.cs
+++ FakeHttpClient/FakeHttpClient.Tests/UnitTest1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using FakeHttpClient;
 using NUnit.Framework;
 
 namespace Tests
@@ -12,6 +16,30 @@ namespace Tests
         [Test]
         public void Test1()
         {
+            Console.WriteLine("Hello World!");
+
+            MockableHttpMessageHandler messageHandler = new MockableHttpMessageHandler();
+            //messageHandler
+            //    .When(HttpMethod.Get).WhenUri("/sdfkljasl/asdfa/das")
+            //    .Execute()
+            //    .Throw()
+            //    .Respond()
+
+            messageHandler.BuildRule().When((req) => req.RequestUri.AbsoluteUri == "https://www.google.com/")
+                .Use(r => { r.StatusCode = HttpStatusCode.Accepted; });
+            messageHandler.BuildRule().When((req) => req.RequestUri.AbsoluteUri == "https://www.yahoo.com/")
+                .Use(r => { r.StatusCode = HttpStatusCode.BadRequest; });
+
+            HttpClient client = new HttpClient(messageHandler);
+            var response1 = client.SendAsync(new HttpRequestMessage() { RequestUri = new Uri("https://www.google.com") })
+                .Result;
+            Console.WriteLine("The response code is " + response1.StatusCode);
+
+
+            var response = client.SendAsync(new HttpRequestMessage() { RequestUri = new Uri("https://www.yahoo.com") })
+                .Result;
+            Console.WriteLine("The response code is " + response.StatusCode);
+
             Assert.Pass();
         }
     }
