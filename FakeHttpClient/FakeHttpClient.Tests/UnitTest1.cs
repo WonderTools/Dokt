ff0ef1113e72d4040dd50b3f51d0102d61b02668
Devbrat Singh <32700744+devbrat11@users.noreply.github.com>
2019-02-26 09:40:28 +0530

    Dev (#1)
    * New features added
    
    * New features added

--- FakeHttpClient/FakeHttpClient.Tests/UnitTest1.cs
+++ FakeHttpClient/FakeHttpClient.Tests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace FakeHttpClient.Tests
     public class Tests
     {
         [Test]
-        public void When_test()
+        public void When_uri_then_status_code()
         {
             MockableHttpMessageHandler messageHandler = new MockableHttpMessageHandler();
 
@@ -26,6 +26,70 @@ namespace FakeHttpClient.Tests
             var response2 = client.SendAsync(new HttpRequestMessage() { RequestUri = new Uri("https://www.yahoo.com") })
                 .Result;
             Assert.AreEqual(HttpStatusCode.BadRequest, response2.StatusCode);
+        }
+
+        [Test]
+        public void When_uri_starts_with_then_status_code()
+        {
+            MockableHttpMessageHandler messageHandler = new MockableHttpMessageHandler();
+
+            messageHandler.BuildRule().WhenUriStartsWith("https")
+                .UseStatusCode(HttpStatusCode.Accepted);
+            messageHandler.BuildRule().WhenUriStartsWith("http")
+                .UseStatusCode(HttpStatusCode.BadRequest);
+
+            HttpClient client = new HttpClient(messageHandler);
+            var response1 = client.SendAsync(new HttpRequestMessage() { RequestUri = new Uri("https://www.google.com") })
+                .Result;
+            Assert.AreEqual(HttpStatusCode.Accepted, response1.StatusCode);
+
+
+            var response2 = client.SendAsync(new HttpRequestMessage() { RequestUri = new Uri("http://www.yahoo.com") })
+                .Result;
+            Assert.AreEqual(HttpStatusCode.BadRequest, response2.StatusCode);
+        }
+
+        [Test]
+        public void When_uri_contains_with_then_status_code()
+        {
+            MockableHttpMessageHandler messageHandler = new MockableHttpMessageHandler();
+
+            messageHandler.BuildRule().WhenUriContains("test")
+                .UseStatusCode(HttpStatusCode.Accepted);
+            messageHandler.BuildRule().WhenUriContains("1234")
+                .UseStatusCode(HttpStatusCode.BadRequest);
+
+            HttpClient client = new HttpClient(messageHandler);
+            var response1 = client.SendAsync(new HttpRequestMessage() { RequestUri = new Uri("https://www.test.com") })
+                .Result;
+            Assert.AreEqual(HttpStatusCode.Accepted, response1.StatusCode);
+
+
+            var response2 = client.SendAsync(new HttpRequestMessage() { RequestUri = new Uri("http://www.1234.com") })
+                .Result;
+            Assert.AreEqual(HttpStatusCode.BadRequest, response2.StatusCode);
+        }
+
+        [Test]
+        public void When_uri_port_then_status_code()
+        {
+            MockableHttpMessageHandler messageHandler = new MockableHttpMessageHandler();
+
+            messageHandler.BuildRule().WhenUriPort(80)
+                .UseStatusCode(HttpStatusCode.Accepted);
+            messageHandler.BuildRule().WhenUriPort(100)
+                .UseStatusCode(HttpStatusCode.InternalServerError);
+
+            HttpClient client = new HttpClient(messageHandler);
+            var requestUri = new Uri("https://ip:80//www.test.com");
+            var response1 = client.SendAsync(new HttpRequestMessage() { RequestUri = requestUri })
+                .Result;
+            Assert.AreEqual(HttpStatusCode.Accepted, response1.StatusCode);
+
+
+            var response2 = client.SendAsync(new HttpRequestMessage() { RequestUri = new Uri("http://ip:100/www.1234.com") })
+                .Result;
+            Assert.AreEqual(HttpStatusCode.InternalServerError, response2.StatusCode);
         }
     }
 }
