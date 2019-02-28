68a30d646ed764cba7f0b31a0fd7648a8b834c8c
Devbrat Singh <32700744+devbrat11@users.noreply.github.com>
2019-02-28 09:36:06 +0530

    Refactored the code (#2)

--- FakeHttpClient/FakeHttpClient/Rule.cs
+++ FakeHttpClient/FakeHttpClient/Rule.cs
@@ -1,47 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Net.Http;
 
 namespace FakeHttpClient
 {
     public class Rule
     {
-        List<Func<HttpRequestMessage, bool>> _entryConditions = new List<Func<HttpRequestMessage, bool>>();
+        Func<Exception> _exceptionFactory;
         List<Action<HttpResponseMessage>> _modifiers = new List<Action<HttpResponseMessage>>();
-        private Func<Exception> _exceptionFactory;
+        List<Predicate<HttpRequestMessage>> _entryConditions = new List<Predicate<HttpRequestMessage>>();
 
-        public Rule WhenUri(string uri)
+        public void AddEntryCondition(Predicate<HttpRequestMessage> entryCondition)
         {
-            _entryConditions.Add(x=>x.RequestUri.AbsoluteUri==uri);
-            return this;
+            _entryConditions.Add(entryCondition);
         }
 
-        public Rule WhenUriStartsWith(string segment)
+        public void AddModifier(Action<HttpResponseMessage> modifier)
         {
-            _entryConditions.Add(x => x.RequestUri.OriginalString.StartsWith(segment));
-            return this;
-        }
-
-        public Rule WhenUriContains(string segment)
-        {
-            _entryConditions.Add(x => x.RequestUri.AbsoluteUri.Contains(segment));
-            return this;
-        }
-
-        public Rule WhenUriPort(int port)
-        {
-            _entryConditions.Add(x => x.RequestUri.Port.Equals(port));
-            return this;
-        }
-        
-        //When Uri Predicate<string>
-        //When UriSheme
-
-        public Rule UseStatusCode(HttpStatusCode statusCode)
-        {
-            _modifiers.Add(x => { x.StatusCode = statusCode; });
-            return this;
+            _modifiers.Add(modifier);
         }
 
         public bool IsMatch(HttpRequestMessage request)
---
+++ FakeHttpClient/FakeHttpClient/RuleUriExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FakeHttpClient
+{
+    public static class RuleUriExtensions
+    {
+        public static Rule WhenUri(this Rule rule, string uri)
+        {
+            rule.AddEntryCondition(x => x.RequestUri.AbsoluteUri == uri);
+            return rule;
+        }
+
+        public static Rule WhenUriStartsWith(this Rule rule, string segment)
+        {
+            rule.AddEntryCondition(x => x.RequestUri.OriginalString.StartsWith(segment));
+            return rule;
+        }
+
+        public static Rule WhenUriContains(this Rule rule, string segment)
+        {
+            rule.AddEntryCondition(x => x.RequestUri.AbsoluteUri.Contains(segment));
+            return rule;
+        }
+
+        public static Rule WhenUriPort(this Rule rule, int port)
+        {
+            rule.AddEntryCondition(x => x.RequestUri.Port.Equals(port));
+            return rule;
+        }
+    }
+}
